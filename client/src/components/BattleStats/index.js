ea6941c74bef2e1a95d4ec8c3dfb4509c47199e2
Andre Entrekin <andre.entrekin@gmail.com>
2020-03-02 14:15:24 -0600

    battle random finished

--- client/src/components/BattleStats/index.js
+++ client/src/components/BattleStats/index.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import "./BattleStats.css"
 
-function BattleStats({ heroHP, enemyHP, heroMaxHP, enemyMaxHP }) {
+function BattleStats({ heroHP, heroMaxHP, enemy }) {
+    console.log('this is enemey in battle stats!!', enemy)
     return (
         <>
             <div className="heroBox">
                 <p>HeroName</p>
                 <progress className="heroBar" id="file" value={heroHP} max={heroMaxHP}></progress>
             </div>
-            <div className="heroImg">
-            </div>
+            <img src="https://i.imgur.com/7rWOaEN.png" className="heroImg" />
             <div className="enemyBox">
-                <p>EnemyName</p>
-                <progress className="enemyBar" id="file" value={enemyHP} max={enemyMaxHP} ></progress>
+                <p>{enemy.enemyName}</p>
+                <progress className="enemyBar" id="file" value={enemy.enemyHP} max={enemy.enemyMaxHP} ></progress>
             </div>
-            <div className="enemyImg">
-            </div>
+
+            <img src={enemy.enemySrc} className="enemyImg" />
+
         </>
     )
 }
--- client/src/pages/Character.js
+++ client/src/pages/Character.js
@@ -6,10 +6,7 @@ import characters from "./character.json"
 
 
 
-function handleButtonChoose() {
-    console.log("clicked Choose");
 
-}
 
 
 
@@ -18,7 +15,9 @@ class Character extends Component {
     state = {
         data: characters,
         curCharacter: {},
-        curDescription: "This is a text"
+        curDescription: "This is a text",
+        heroIndex: 1,
+        coolDown: false
 
     }
     componentDidMount() {
@@ -27,78 +26,88 @@ class Character extends Component {
         // console.log(this.state.curCharacter)
     }
 
-    characterSelect = (e) => {
-        // e.preventDefault()
-        // for (let i = 0; i < characters.length; i++) {
-        //     this.setState({
-        //         curCharacter: characters[i].url,
-        //         curDescription: characters[i].description
-        //     })
+    handleButtonChoose = () => {
+        console.log("clicked this is current hero Index!!!", this.state.heroIndex);
+        window.location.href = "movement"
+        // const char = document.getElementsByClassName("active").value()
+        // console.log(char);
 
-        // }
+    }
 
+    setCoolDown = () => {
 
+        const self = this
+        this.setState({ coolDown: true }, function () {
+            setTimeout(function () {
+                self.setState({ coolDown: false })
+            }, 1000)
+        })
+    }
 
-        // var cur = document.getElementsByClassName("active")
-        // console.log("activ1", cur)
-        // var newClass = cur[0].children[0].alt
-        // console.log("activ2", newClass);
-        // var arr = this.state.data;
-        // console.log(arr);
-        // for (let i = 0; i < arr.length; i++) {
-        //     console.log(arr[i], arr[i + 2]);
-        //     if (newClass == arr[i].class) {
-        //         this.setState({ curCharacter: arr[i] });
-        //         console.log(this.state.curCharacter)
-        //     }
-        // }
+    handleNextHero = (direction) => {
+        console.log('go to new hero', direction)
+        this.setCoolDown()
+
+        if (direction === 'next') {
+            if (this.state.heroIndex === 4) {
+                this.setState({ heroIndex: 1 })
+            } else {
+                this.setState({ heroIndex: this.state.heroIndex + 1 })
+            }
+        }
+
+        if (direction === 'previous') {
+            if (this.state.heroIndex === 1) {
+                this.setState({ heroIndex: 4 })
+            } else {
+                this.setState({ heroIndex: this.state.heroIndex - 1 })
+            }
+        }
     }
 
     render() {
+        console.log('this is our state', this.state)
         return (
             <div className="charContain">
-                <div className="row">
-                    <div className="col-md-6">
 
-                        <div id="carouselExampleControls" class="sprite carousel slide" data-ride="carousel" data-interval="false" >
-                            <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                    <img class="d-block h-100" src="https://i.imgur.com/huVIPkL.png" alt="Warrior" />
-                                    <div className="testText"> 1This is test 1</div>
-                                </div>
-                                <div class="carousel-item">
-                                    <img class="d-block h-100" src={this.state.curCharacter} alt="Archer" />
-                                </div>
-                                <div class="carousel-item">
-                                    <img class="d-block h-100" src="https://i.imgur.com/bKvuY6T.png" alt="Warrior" />
-                                    <div className="testText"> 2This is test 2</div>
-                                </div>
-                                <div class="carousel-item">
-                                    <img class="d-block h-100" src="https://i.imgur.com/pIHmyE4.png" alt="Mage" />
-                                    <div className="testText"> 3This is test 3</div>
-                                </div>
-                            </div>
-                            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="carousel-control-next" onClick={this.characterSelect} href="#carouselExampleControls" role="button" data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                            </a>
+                <div id="carouselExampleControls" class="carousel slide" data-interval="false" >
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block h-100 sprite" value="1" src="https://i.imgur.com/huVIPkL.png" alt="Archer" />
                         </div>
-
-                        <button onClick={handleButtonChoose} className="choose">Choose Character</button>
-
+                        <div class="carousel-item">
+                            <img class="d-block h-100 sprite" value="2" src="https://i.imgur.com/fCofyAs.png" alt="Warrior" />
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block h-100 sprite" value="3" src="https://i.imgur.com/bKvuY6T.png" alt="Warrior" />
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block h-100 sprite" value="4" src="https://i.imgur.com/pIHmyE4.png" alt="Mage" />
+                        </div>
                     </div>
-                    <div className="col-md-6">
-                        <Scroll style={{ zIndex: '-1' }}
-                            text={this.state.curDescription}
-                        />
-                    </div>
+
+                    {!this.state.coolDown ? (
+                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev" onClick={() => { this.handleNextHero('previous') }}>
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only" >Previous</span>
+                        </a>
+                    ) : ''}
+
+
+                    {!this.state.coolDown ? (
+                        <a class="carousel-control-next" onClick={() => { this.handleNextHero('next') }} href="#carouselExampleControls" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only" >Next</span>
+                        </a>
+                    ) : ''}
+
                 </div>
 
+                <button onClick={this.handleButtonChoose} className="choose">Choose Character</button>
+
             </div>
+
+
         )
     }
 }
